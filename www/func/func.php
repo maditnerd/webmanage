03e6159ca3831d12281540a2aa5d821381a8f7a4
sarrailh.remi <sarrailh.remi@d3d7e8a0-cf54-417b-4601-6a8c3857912d>
2012-04-08 19:05:45 +0000

    Login fonctionnel (sauf sur Android)
    Invite de commande en javascript rajouté!
    
    L'encodage est incorrecte et il faut taper c:\windows\system32\"commande" pour avoir accès aux commandes de base
    
    Le path est défini donc le problème ne vient pas de là
    
    Evidemment même si on a accès à tout les répertoires on ne peut pas faire cd.. , vu que les commandes précédentes ne sont pas enregistrés.

--- www/func/func.php
+++ www/func/func.php
@@ -96,6 +96,7 @@ return $result;
 function header_show($titre,$css)
 {
 echo "
+<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN' 'http://www.w3.org/TR/html4/loose.dtd'>
 <html>
 <head>
 <title>Webmanage : ".$titre."</title>
