798ecdf1c330fc5c0a78100606c319a942bc8047
sarrailh.remi <sarrailh.remi@d3d7e8a0-cf54-417b-4601-6a8c3857912d>
2012-04-09 11:32:21 +0000

    admin.php
    Afficher des logins et création du login fonctionnel
    Vérification primaire (pas de " ou ' dans les logins)
    
    class.login.php
    Nouvelles fonctions
    show_users (affiche les utilisateurs)
    delete_login (efface un login)
    check_login (verifie qu'un login existe)
    update_password (mis à jour d'un mot de passe)
    
    func.php
    Nouvelle fonctions
    menu_img_get (identique à menu_img mais ne met pas automatiquement le .php afin de permettre de rajouter des valeurs get ex: img.php?name=ee)
    
    menu_usr (Objet pour les utilisateur dérivé de menu_img
    
    back_user (Revenir à admin.php)
    
    Création de user.php
    Permet de modifier un utilisateur particulier (suppression/mot de passe)
    
    dos.php
    Dérivation du code dans js/dos.js

--- trunk/www/admin.php
+++ trunk/www/admin.php
@@ -12,40 +12,94 @@ if($log->logincheck($_SESSION['loggedin'], "logon", "password", "useremail") == 
 	else{
 //LOGIN END
 
-
+// PASSWORD GENERATION
 
 header_show("Utilisateurs","style2");
 
+//////////////////////
+//CREATE USER
+/////////////////////
 
+// Vérifie que les informations ont été correctement entrer
 
+$pass = trim($_POST['pass']);
+$login = $_POST['login'];
+$login = str_replace('"','',$login);
+$login = str_replace("'",'',$login);
+$login = trim($login);
+$pass_confirm = trim($_POST['pass_confirm']);
 
+if ($pass == "" || $login == "" || $pass_confirm == "")
+	{
+		// DO NOTHING (car on ne peut pas deviner si y a eu un post
+	}
+else
+	{
+		
+	if ($pass == $pass_confirm)
+		{		
+			//VERIFICATION existance du login
+			$log = new logmein();
+			$login_existence = $log->check_login($login);
+			if ($login_existence["useremail"] == $login)
+				{
+					title('<img src="/img/info.png" height="32" width="32" ><font color="#990000">Ce login existe déjà</font>');
+				}
+			else
+				{
+				$username = $login;
+				$password = md5($pass);
+				$log = new logmein();
+				$log->create_login($username, $password);
+				}
+		}
+	else
+		{
+			title('<img src="/img/info.png" height="32" width="32" > <font color="#990000">Vous avez rentré un mot de passe différent</font>');
+		}	
+	}
+	
+/////////////////////////
+//CREATE USER END
+////////////////////////
 
-if (isset($_POST['pass']) AND isset($_POST['login']))
-{
-$username = $_POST['login'];
-$password = md5($_POST['pass']);
-$log = new logmein();
-$log->create_login($username, $password);
-}
-
-else 
-{
-}
 title("Utilisateurs");
-title("Création d'un utilisateur");
-
+/////////////////////////
+//SHOW USER
+////////////////////////
+$log = new logmein();
+$result = $log->show_users();
+echo "<table align=center><tr>";
+while ($row = mysql_fetch_assoc($result))
+		{
+		 $username = $row["useremail"] ;
+		
+		 menu_usr($username);
+		}
+		
+/////////////////////////
+//SHOW USER END
+////////////////////////	
+	
+echo "</tr></table>";
+title("Nouveau Utilisateur");
 echo '
-
 <form method="post">
-    <p align=center>
-        Login : &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-		<input type="text" name="login"><br />
-        Mot de passe :
-		<input type="text" name="pass"><br />
-	 ';
+<table align=center><tr><td>
+        Identifiant : &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+		<input type="text" name="login"></td></tr><td>
+		Mot de passe : &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+		<input type="password" name="pass"></td></tr><td>
+		Confirmer le mot de passe :
+		<input type="password" name="pass_confirm"><br /></td></tr><td align=center>
+		<INPUT  type="submit" value="Enregistrer" />
+		
+</td></tr></table>
+</form>
+';
 
- submit();
 
+back();
 footer_show();
 }
 ?>
---
+++ trunk/www/user.php
@@ -0,0 +1,83 @@
+<?php
+include("/func/func.php");
+include("/func/class.login.php");
+
+// LOGIN START
+$log = new logmein();
+$log->encrypt = true; //set encryption
+//parameters are(SESSION, name of the table, name of the password field, name of the username field)
+if($log->logincheck($_SESSION['loggedin'], "logon", "password", "useremail") == false){
+  echo '<meta http-equiv="refresh" content="0; URL=index.php">';
+}
+	else{
+//LOGIN END
+$user = $_GET["user"];
+
+// Vérifie sur un utilisateur a été spécifié
+if (!isset($user)) 
+{
+  echo '<meta http-equiv="refresh" content="0; URL=admin.php">';
+}
+
+else
+ {
+
+////////////////////////
+// DELETE USER
+//////////////////////// 
+ 
+if ($_GET["delete"] == 1)
+{
+$log = new logmein();
+$log->delete_login($user);
+
+}
+
+/////////////////////////
+/// UPDATE PASSWORD
+/////////////////////////
+if ($_POST["pass"] == "")
+{
+// DO NOTHING
+}
+else
+{
+if ($_POST["pass"] == $_POST["pass_confirm"])
+{
+$password = md5($_POST["pass"]);
+$log = new logmein();
+$log->update_password($user,$password);
+}
+else
+{
+	title('<img src="/img/info.png" height="32" width="32" > <font color="#990000">Vous avez rentré un mot de passe différent</font>');
+	}
+}
+
+header_show($user,"style2");
+
+echo '
+<form method="post">
+';
+title('<INPUT style="background-color:transparent; border:none; text-align:center; font-size:150%; color: #333333;" type="text" value="'.$user.'" name="computername">');
+
+echo '
+
+<table align=center><tr>';
+menu_img_get("delete","user.php?user=".$user."&delete=1","Supprimer l'utilisateur");
+
+echo '<td>
+        Mot de passe : &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+		<input type="password" name="pass"><br />
+		Confirmer le Mot de passe :
+		<input type="password" name="pass_confirm"><br /></td></tr><tr><td></td><td align=center>
+		<INPUT  type="submit" value="Enregistrer" />
+		
+		</td></tr></table>
+		</form>
+';
+
+ back_user();
+ }
+ }
+ ?>
